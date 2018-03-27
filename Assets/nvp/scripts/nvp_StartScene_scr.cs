126463c35763f8fac21dc7fc357162806de196ac
Bernhard Rubow <brubow@newvisionsproject.de>
2018-03-28 00:18:36 +0200

    Added Start and GameOver Scene.

---
+++ Assets/nvp/scripts/nvp_StartScene_scr.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nvp_StartScene_scr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(Input.touchCount);
+		if(Input.touchCount > 0){
+			SceneManager.LoadScene("game");
+		}
+	}
+}
