6962fdc115322e44f9d9b94b9a6520b9d24eb699
Bernhard Rubow <brubow@newvisionsproject.de>
2018-03-31 00:41:35 +0200

    Introduced new state for handling out of bounds situations.

--- Assets/nvp/scripts/nvp_ball_scr.cs
+++ Assets/nvp/scripts/nvp_ball_scr.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using newvisionsproject.managers.events;
 
-namespace newvisionsproject.zong {
-  public class nvp_Ball_scr : MonoBehaviour {
+namespace newvisionsproject.zong
+{
+  /**
+  * This class is responsible for bringing the ball to
+  * live. It the class untiy accesses in the game. */
+  public class nvp_Ball_scr : MonoBehaviour
+  {
 
-    // +++ inspektor fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++ inspector fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     public float MaxHorizontalOffset;
     public Vector3 Direction;
     public float Speed;
@@ -15,38 +20,48 @@ namespace newvisionsproject.zong {
 
 
 
-    // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // +++ private fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     private System.Action stateTick;
-    private IState state;
-    Dictionary<string, IState> states;
 
 
 
 
-    // +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    void Start () {
-      states = CreateStates(this.gameObject);
-      state = states["moving"].SetAsNextState();
+    // +++ public fields ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public IState State;
+    public Dictionary<BallStates, IState> States;
+
+
+
+
+    // +++ unity callbacks ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    void Start()
+    {
+      States = CreateStates(this.gameObject);
+      State = States[BallStates.outOfBounds].SetAsNextState();
     }
-    
-    void Update () {
+
+    void Update()
+    {
       // update the currently assigned state 
-      state.Tick();
+      State.Tick();
     }
 
-    void OnTriggerEnter(Collider other){
-      if(other.tag == "wall") nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallHitsWall, this, null);
-      if(other.tag == "player") nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallHitsPlayer, this, null);
-    }	
+    void OnTriggerEnter(Collider other)
+    {
+      if (other.tag == "wall") nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallHitsWall, this, null);
+      if (other.tag == "player") nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallHitsPlayer, this, null);
+    }
 
 
 
 
     // +++ methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    Dictionary<string, IState> CreateStates(GameObject go){
-      var s = new Dictionary<string, IState>();
+    Dictionary<BallStates, IState> CreateStates(GameObject go)
+    {
+      var s = new Dictionary<BallStates, IState>();
 
-      s.Add("moving", new StateMoving(go));
+      s.Add(BallStates.Moving, new StateMoving(go));
+      s.Add(BallStates.outOfBounds, new StateOutOfBounds(go));
 
       return s;
     }
@@ -56,12 +71,26 @@ namespace newvisionsproject.zong {
 
 
   // +++ states +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  public interface IState{
-    void Tick();
-    
+  public interface IState
+  {
     IState SetAsNextState();
+    void Tick();
+    void OnEnter();   
+    void OnUpdate();
+    IState OnExitTo(BallStates nextState);
   }
 
+  /**
+  * This is an enumeration over the states
+  * the ball can adopt */
+  public enum BallStates{
+    Moving,
+    outOfBounds
+  }
+
+  /**
+  * This state describes the behavior of
+  * the ball when it is moving */
   public class StateMoving : IState
   {
     // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -70,79 +99,173 @@ namespace newvisionsproject.zong {
     private float maxHorizontalOffset;
     private Transform transform;
     private System.Action action;
+    private nvp_Ball_scr ballScript;
 
 
 
 
     // +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    public StateMoving(GameObject go){
+    public StateMoving(GameObject go)
+    {
       // get observed component
-      var script = go.GetComponent<nvp_Ball_scr>();
+      ballScript = go.GetComponent<nvp_Ball_scr>();
 
-      // collect state references
-      speed = script.Speed;
-      direction = script.Direction;
-      transform = go.transform;
-      maxHorizontalOffset = script.MaxHorizontalOffset;
-
-      nvp_EventManager_scr.INSTANCE.SubscribeToEvent(GameEvents.onBallHitsWall, OnBallHitsWall);
-      nvp_EventManager_scr.INSTANCE.SubscribeToEvent(GameEvents.onBallHitsPlayer, OnBallHitsPlayer);
+      // collect references from the ballscript
+      // that are needed to access within this behavior
+      speed = ballScript.Speed;
+      direction = ballScript.Direction;
+      transform = ballScript.transform;
+      maxHorizontalOffset = ballScript.MaxHorizontalOffset;      
     }
 
 
 
 
     // +++ event handler ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    void OnBallHitsPlayer(object sender, object eventArgs){
+    void OnBallHitsPlayer(object sender, object eventArgs)
+    {
       direction.y = Mathf.Sign(direction.y) * -1;
       direction.x = Random.Range(-2.0f, 2.0f);
       direction.Normalize();
     }
-    void OnBallHitsWall(object sender, object eventArgs){
+    void OnBallHitsWall(object sender, object eventArgs)
+    {
       direction.x *= -1;
     }
 
 
 
     // +++ interface methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-      public IState SetAsNextState()
+    public void Tick()
+    {
+      action();
+    }
+
+    public IState SetAsNextState()
+    {
+      action = OnEnter;
+      return this;
+    }
+
+    public void OnEnter()
+    {
+      nvp_EventManager_scr.INSTANCE.SubscribeToEvent(GameEvents.onBallHitsWall, OnBallHitsWall);
+      nvp_EventManager_scr.INSTANCE.SubscribeToEvent(GameEvents.onBallHitsPlayer, OnBallHitsPlayer);
+
+      action = OnUpdate;
+      action();
+    }
+
+    public void OnUpdate()
+    {
+      transform.Translate(direction * speed * Time.deltaTime, Space.World);
+      CheckTransitionTo_MovingState();
+    }
+
+    public IState OnExitTo(BallStates nextState)
+    {
+      // do any cleanup here
+
+      // unsubscribe form former subscribtions to particular events
+      nvp_EventManager_scr.INSTANCE.UnsubscribeFromEvent(GameEvents.onBallHitsWall, OnBallHitsWall);
+      nvp_EventManager_scr.INSTANCE.UnsubscribeFromEvent(GameEvents.onBallHitsPlayer, OnBallHitsPlayer);
+
+      // get the next state and return it to provide
+      // fluent configuration
+      return ballScript.States[nextState];
+    }
+
+
+
+
+    // +++ check methods for state transitions ++++++++++++++++++++++++++++++++++++++++++++++++++++
+    private void CheckTransitionTo_MovingState()
+    {
+      if (Mathf.Abs(transform.position.y) > 25)
       {
-          action = OnEnter;
-          return this;
+        nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallOutOfBounds, this, this.transform.position.y);
+        ballScript.State = OnExitTo(BallStates.outOfBounds).SetAsNextState();
       }
-
-      public void Tick()
-      {
-        action();
-      }
-
-
-
-
-      // +++ state actions ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-      void OnEnter(){
-        action = OnUpdate;
-        action();
-      } 
-
-      void OnUpdate(){
-        transform.Translate(direction * speed * Time.deltaTime, Space.World);
-
-        if(Mathf.Abs(transform.position.y)> 25){
-
-          nvp_EventManager_scr.INSTANCE.InvokeEvent(GameEvents.onBallOutOfBounds, this, this.transform.position.y);
-
-          // reset position
-          // TODO: Add extra state
-          transform.position = Vector3.zero;
-        }
-      } 
-
-      void OnExit(){
-
-      }  
-
+    }
   }
 
+  /**
+  * This state describes the behavior of
+  * the ball, when the player misses the
+  * ball and it is getting behind the player */
+  public class StateOutOfBounds : IState
+  {
+    // +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    Transform transform;
+    System.Action action;
+    nvp_Ball_scr ballScript;
+    bool exitState;
 
+
+
+
+    // +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public StateOutOfBounds(GameObject go)
+    {
+      // collect references and store them localy
+      // for ease of use
+      ballScript = go.GetComponent<nvp_Ball_scr>();
+      transform = ballScript.transform;
+    }
+
+
+
+
+    // +++ interface methods ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public void Tick()
+    {
+      action();
+    }
+    
+    public IState SetAsNextState()
+    {
+      action = OnEnter;
+      return this;
+    }
+
+    public void OnEnter()
+    {   
+      // reset position
+      transform.position = Vector3.zero;
+      exitState = false;
+
+      // start waiting for some amount of time
+      ((MonoBehaviour)ballScript).StartCoroutine(Idle());
+
+      // internal state change
+      action = OnUpdate;
+    }
+
+    public void OnUpdate()
+    {
+      // check if the condition for transition to the moving
+      // state is fullfilled
+      if(exitState){
+        ballScript.State = OnExitTo(BallStates.Moving).SetAsNextState();
+      }
+    }
+
+    public IState OnExitTo(BallStates nextState)
+    {
+      // Do cleanup here
+
+      // get the next state and return it to provide
+      // fluent configuration
+      return  ballScript.States[nextState];;
+    }
+
+
+
+
+    // +++ coroutines +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    IEnumerator Idle(){
+      yield return new WaitForSeconds(1);
+      exitState = true;
+    }
+  }
 }
