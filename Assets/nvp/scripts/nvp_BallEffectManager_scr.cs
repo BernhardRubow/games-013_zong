c43c0a1373fdcceb0aeef7c5d3d30b8bfcf87d34
Bernhard Rubow <brubow@newvisionsproject.de>
2018-04-04 13:57:12 +0200

    Added more effects and game background music.

---
+++ Assets/nvp/scripts/nvp_BallEffectManager_scr.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using newvisionsproject.managers.events;
+
+namespace newvisionsproject.zong.effects 
+{
+	public class nvp_BallEffectManager_scr : MonoBehaviour {
+
+		[SerializeField] ParticleSystem ballParticleSystem;
+		[SerializeField] Color upperPlayerColor;
+		[SerializeField] Color lowerPlayerColor;
+
+		// Use this for initialization
+		void Start () {
+			nvp_EventManager_scr.INSTANCE.SubscribeToEvent(GameEvents.onBallHitsPlayer, OnBallHitsPlayer);
+		}
+
+		void OnBallHitsPlayer(object sender, object eventArgs){
+			var main = ballParticleSystem.main;
+			float yPos = ((nvp_Ball_scr)sender).gameObject.transform.position.y;
+			if(yPos < 0) main.startColor = lowerPlayerColor;
+			else main.startColor = upperPlayerColor;
+		}
+	}
+
+}
