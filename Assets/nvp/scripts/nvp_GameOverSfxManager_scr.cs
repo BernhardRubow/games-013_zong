e8b1626711e38cd35310e36cf0314568b5a77669
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2018-04-13 22:15:03 +0200

    Added SFX to GameOver

---
+++ Assets/nvp/scripts/nvp_GameOverSfxManager_scr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nvp_GameOverSfxManager_scr : MonoBehaviour {
+
+	// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	[SerializeField] AudioClip winnerClip;
+	[SerializeField] AudioClip loserClip;
+
+
+
+	// +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void Start () {
+		StartCoroutine(PlayMusicSequence());
+	}
+
+	// +++ coroutines +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	IEnumerator PlayMusicSequence(){
+		AudioSource audio = this.GetComponent<AudioSource>();
+
+		yield return new WaitForSeconds(0.2f);
+		audio.clip = winnerClip;
+		audio.Play();
+
+		yield return new WaitForSeconds(winnerClip.length);
+		audio.clip = loserClip;
+		audio.Play();
+		
+	}
+}
