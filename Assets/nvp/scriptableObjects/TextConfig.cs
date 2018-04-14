6be6fdfe012a5f0ee99ab8e73d8d686a9e229482
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2018-04-14 14:32:43 +0200

    Working on credits scene

---
+++ Assets/nvp/scriptableObjects/TextConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Zong/Configuration/TextMover")]
+public class TextConfig : ScriptableObject {
+
+	public float moveSpeed;
+	public float startAnmationY;	
+}
---
+++ Assets/nvp/scripts/nvp_TextMover_scr.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nvp_TextMover_scr : MonoBehaviour {
+
+	// +++ fields +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	[SerializeField] TextConfig textConfig;
+
+	// +++ life cycle +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.up * textConfig.moveSpeed * Time.deltaTime, Space.World);
+	}
+}
